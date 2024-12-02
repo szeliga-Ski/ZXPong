3a628ce7e51bf99534ee398ec4fb4d4ff3152dc4
szeliga-Ski <szeliga@hotmail.com>
2024-12-02 18:44:02 +0000

    Amended path to "pasmo.exe" for use on laptop

--- build.cmd
+++ build.cmd
@@ -1,4 +1,6 @@
 cls
 rem "D:/Users/Mark/Retro Stuff/ZX Spectrum/06 - Development/Assemblers/pasmo-0.5.5/pasmo.exe" --name ZX-Pong --tapbas main.asm Pong.tap --Pong.log
-"D:/Users/Mark/Retro Stuff/ZX Spectrum/06 - Development/Assemblers/pasmo-0.5.5/pasmo.exe" --name ZX-Pong --tap main.asm pong.tap
+rem "D:/Users/Mark/Retro Stuff/ZX Spectrum/06 - Development/Assemblers/pasmo-0.5.5/pasmo.exe" --name ZX-Pong --tap main.asm pong.tap
+
+"c:/Users/szeli/OneDrive/Retro Stuff/ZX Spectrum/06 - Development/Assemblers/pasmo-0.5.5/pasmo.exe" --name ZX-Pong --tap main.asm pong.tap
 copy /b PongLoader.tap+PongScr.tap+pong.tap ZX-Pong.tap
