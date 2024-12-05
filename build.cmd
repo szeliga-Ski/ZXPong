b8db49189230288fa01c13be6268ff886380a802
szeliga-Ski <szeliga@hotmail.com>
2024-12-05 20:42:18 +0000

    added date of PATH change to build.cmd

--- build.cmd
+++ build.cmd
@@ -4,5 +4,6 @@ cls
 rem "D:/Users/Mark/Retro Stuff/ZX Spectrum/06 - Development/Assemblers/pasmo-0.5.5/pasmo.exe" --name ZX-Pong --tapbas main.asm Pong.tap --Pong.log
 rem "D:/Users/Mark/Retro Stuff/ZX Spectrum/06 - Development/Assemblers/pasmo-0.5.5/pasmo.exe" --name ZX-Pong --tap main.asm pong.tap
 
+; December 2024
 "c:/Users/szeli/OneDrive/Retro Stuff/ZX Spectrum/06 - Development/Assemblers/pasmo-0.5.5/pasmo.exe" --name ZX-Pong --tap main.asm pong.tap
 copy /b PongLoader.tap+PongScr.tap+pong.tap ZX-Pong.tap
