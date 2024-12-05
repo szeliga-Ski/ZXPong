4a886b32b3bc7790854ef1e38d65cd5dbfab7a0d
szeliga-Ski <szeliga@hotmail.com>
2024-12-05 20:25:21 +0000

    test

--- build.cmd
+++ build.cmd
@@ -1,3 +1,5 @@
+; build for Garcia book
+
 cls
 rem "D:/Users/Mark/Retro Stuff/ZX Spectrum/06 - Development/Assemblers/pasmo-0.5.5/pasmo.exe" --name ZX-Pong --tapbas main.asm Pong.tap --Pong.log
 rem "D:/Users/Mark/Retro Stuff/ZX Spectrum/06 - Development/Assemblers/pasmo-0.5.5/pasmo.exe" --name ZX-Pong --tap main.asm pong.tap
